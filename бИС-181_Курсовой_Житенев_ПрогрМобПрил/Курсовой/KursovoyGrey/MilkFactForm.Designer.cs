10b954fc4afad8de47a5e84e4e41a1f87cfe0749
SergeyZhitenev <92933234+SergeyZhitenev@users.noreply.github.com>
2022-01-09 19:01:54 +0300

    Add files via upload

---
+++ бИС-181_Курсовой_Житенев_ПрогрМобПрил/Курсовой/KursovoyGrey/MilkFactForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace KursovoyGrey
+{
+    partial class MilkFactForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MilkFactForm));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbPogr = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbsost = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPogr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 118);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(25, 229);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(475, 126);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Механики";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(21, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ремонт";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(400, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbPogr
+            // 
+            this.pbPogr.Image = ((System.Drawing.Image)(resources.GetObject("pbPogr.Image")));
+            this.pbPogr.Location = new System.Drawing.Point(500, 140);
+            this.pbPogr.Name = "pbPogr";
+            this.pbPogr.Size = new System.Drawing.Size(100, 63);
+            this.pbPogr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPogr.TabIndex = 5;
+            this.pbPogr.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(769, 106);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(119, 97);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(854, 191);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(84, 56);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(779, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(719, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Количество молока на складе";
+            // 
+            // lbsost
+            // 
+            this.lbsost.AutoSize = true;
+            this.lbsost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbsost.Location = new System.Drawing.Point(719, 43);
+            this.lbsost.Name = "lbsost";
+            this.lbsost.Size = new System.Drawing.Size(190, 17);
+            this.lbsost.TabIndex = 10;
+            this.lbsost.Text = "Производство остановлено";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(493, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Время";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(545, 14);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(19, 13);
+            this.lbTime.TabIndex = 12;
+            this.lbTime.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(751, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Начать работу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClStr);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(832, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClStp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Обновить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ClUpd);
+            // 
+            // MilkFactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbsost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pbPogr);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MilkFactForm";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MilkFactForm_FormClosed);
+            this.Load += new System.EventHandler(this.MilkFactForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPogr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbPogr;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbsost;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ бИС-181_Курсовой_Житенев_ПрогрМобПрил/Курсовой/KursovoyGrey/MilkFactForm.cs
@@ -0,0 +1,86 @@
+﻿using MilkFactoryLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KursovoyGrey
+{
+    public partial class MilkFactForm : Form
+    {
+        Loader loader;
+        Warehouse warehouse ;
+        public delegate void DelegateForTime(Label label);
+        DelegateForTime DelTime;
+        Thread t1;
+        public MilkFactForm()
+        {
+            InitializeComponent();
+            DelTime = new DelegateForTime(StartTime);
+            loader = new Loader(pbPogr);
+            warehouse = new Warehouse(loader,textBox1,lbsost);
+        }
+        private void MilkFactForm_Load(object sender, EventArgs e)
+        {
+            t1 = new Thread(LabelTime); // создаем поток  
+            t1.IsBackground = true; // задаем фоновый режым  
+            t1.Priority = ThreadPriority.Lowest; // указываем свмый низкий приоритет  
+            t1.Start(); // 
+            warehouse.mechs.Add(new Mechanic("Иванов", "Иван", 32));
+            warehouse.mechs.Add(new Mechanic("Сидаров", "Дмитрий", 34));
+            dataGridView1.DataSource = warehouse.mechs;
+
+        }
+        void LabelTime()
+        {
+            // безконечный цыкл  
+            while (true)
+            {
+                Invoke(DelTime, lbTime);// запускаем метод с главного потока             
+            }
+        }
+        void StartTime(Label label)
+        {
+            // выводим всегда две цыфры   
+            // (00:00)  
+            string s = DateTime.Now.Hour.ToString("00");
+            s += " : ";
+            s += DateTime.Now.Minute.ToString("00");
+
+            s += " : " + DateTime.Now.Second.ToString("00");
+            label.Text = s;
+        }
+        private void ClStr(object sender, EventArgs e)
+        {
+            lbsost.Text = "Производство работает";
+            warehouse.Open();
+            
+        }
+
+        private void ClStp(object sender, EventArgs e)
+        {
+            lbsost.Text = "Производство остановлено";
+            warehouse.Close();              
+            //dataGridView2.DataSource = warehouse.ot;
+        }
+
+        private void ClUpd(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = warehouse.otchet;
+        }
+
+        private void MilkFactForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(warehouse.IsWork())
+            warehouse.Close();
+            t1.Abort();
+        }
+    }
+}
